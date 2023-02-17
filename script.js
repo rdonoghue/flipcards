b09ff1b6162ea693d04428d643d1b005989403b3
Rob Donoghue <rdonoghue@gmail.com>
2023-02-17 09:05:26 -0500

    Point of frustration

--- script.js
+++ script.js
@@ -4,38 +4,60 @@ const card = document.querySelector(".card");
 const trumpSelector = document.querySelectorAll(".trump-selector");
 var buttonDiv = document.getElementById("buttonList");
 
-// const cardDetails = {};
-// cardDetails["ophelia"] = {
-//   cardname: "Ophelia",
-//   url: "/img/ophelia.png",
-//   angle: -9,
-//   description: "<h2>Inferno</h2>",
-// };
-
-// cardDetails["bevin"] = {
-//   cardname: "Bevin",
-//   url: "/img/bevin.png",
-//   angle: 6,
-//   description: "<h2>Slayer of Monsters</h2>",
-// };
 import { cardDetails } from "./trumpdata.js";
 
+// buildCategories(cardDetails);
 setup(cardDetails);
 shuffle();
 
 console.log("Making sure the script runs");
-console.log(cardpivot());
+
+function buildCategories(cardList) {
+  for (let k in cardList) {
+    if (k.type == "parent") {
+      console.log(k.cardname);
+    }
+    // var newDiv = document.createElement("div");
+    // newDiv.id = cardCategories[k]["category"];
+    // buttonDiv.appendChild(newDiv);
+    // var thisDiv = document.getElementById(cardCategories[k]["category"]);
+    // console.log(cardCategories[k]["category"]);
+    // thisDiv.classList.add("trump-category");
+    // thisDiv.classList.add("trump-button");
+
+    // document
+    //   .getElementById(cardCategories[k]["category"])
+    //   .classList.add("trump-category");
+    // document
+    //   .getElementById(cardCategories[k]["category"])
+    //   .classList.add("trump-button");
+
+    //   var newButton = document.createElement("button");
+    //   newButton.id = k;
+    //   newButton.addEventListener("click", function () {
+    //     console.log("It Worked!");
+    //     console.log("You clicked:", this.id);
+    //     console.log(cardCategories[k]); // pickcard(this.id);
+    //   });
+    //   newButton.innerText = cardCategories[k]["cardname"];
+    //   thisDiv.appendChild(newButton);
+  }
+}
 
 function setup(cardList) {
   for (let k in cardList) {
+    var parentCategory = cardList[k]["parent"];
+    var parentContainer = document.getElementById("buttonDiv");
     var button = document.createElement("BUTTON");
     button.id = k;
     button.innerText = cardDetails[k]["cardname"];
-    buttonDiv.appendChild(button);
+    parentContainer.appendChild(button);
     document.getElementById(k).classList.add("trump-selector");
+    document.getElementById(k).classList.add("trump-button");
+
     document.getElementById(k).addEventListener("click", function () {
       console.log("You clicked:", this.id);
-      pickcard(this.id);
+      pickcard(cardList[k]);
     });
   }
   document.getElementById("pickrandom").addEventListener("click", function () {
@@ -55,24 +77,34 @@ function resetactive(activebutton) {
 }
 
 function shuffle() {
+  var buttonList = document;
   const cardarray = Object.keys(cardDetails);
   console.log(cardarray[Math.floor(Math.random() * cardarray.length)]);
   var picked = cardarray[Math.floor(Math.random() * cardarray.length)];
   pickcard(picked);
 }
-
-function pickcard(cardId) {
-  document.getElementById("cardname").textContent =
-    cardDetails[cardId]["cardname"];
-  document.getElementById("trumpImage").src = cardDetails[cardId]["url"];
+function pickcard(cardDict) {
+  document.getElementById("cardname").textContent = cardDict.cardname;
+  document.getElementById("trumpImage").src = cardDict.url;
   document.getElementById("frontCard").style.transform = "".concat(
     "rotate(" + cardpivot() + "deg)"
   );
-
-  document.getElementById("char-blurb").innerHTML =
-    cardDetails[cardId]["tagline"];
-
-  document.getElementById("char-desc").innerHTML = cardDetails[cardId]["blurb"];
-
-  resetactive(cardId);
+  document.getElementById("char-blurb").innerHTML = cardDict.tagline;
+  document.getElementById("char-desc").innerHTML = cardDict.blurb;
+  console.log(cardDict.id);
+  resetactive(cardDict.id);
 }
+
+// function pickcardold(cardId) {
+//   document.getElementById("cardname").textContent =
+//     cardDetails[cardId]["cardname"];
+//   document.getElementById("trumpImage").src = cardDetails[cardId]["url"];
+//   document.getElementById("frontCard").style.transform = "".concat(
+//     "rotate(" + cardpivot() + "deg)"
+//   );
+//   document.getElementById("char-blurb").innerHTML =
+//     cardDetails[cardId]["tagline"];
+
+//   document.getElementById("char-desc").innerHTML = cardDetails[cardId]["blurb"];
+//   resetactive(cardId);
+// }
--- trumpdata.js
+++ trumpdata.js
@@ -1,58 +1,78 @@
 "use strict";
 
-const cardCategories = {};
-cardCategories["oberon"] = {
+const cardDetails = {};
+cardDetails["oberon"] = {
+  type: "parent",
+  category: "oberon-category",
   cardname: "Oberon",
   url: ".img/oberon-web.png",
   tagline: "King",
   blurb: "King of Amber. Manly man.",
 };
 
-cardCategories["velka"] = {
+cardDetails["velka"] = {
+  type: "parent",
+
+  category: "velka-category",
   cardname: "Velka",
   url: ".img/velka-web.png",
   tagline: "Queen of Dreams",
   blurb: "",
 };
 
-cardCategories["fina"] = {
+cardDetails["fina"] = {
+  type: "parent",
+
+  category: "fina-category",
   cardname: "Fina",
   url: ".img/fina-web.png",
   tagline: "Queen of Mirrors",
   blurb: "",
 };
 
-cardCategories["milesia"] = {
+cardDetails["milesia"] = {
+  type: "parent",
+
+  category: "milesia-category",
   cardname: "Milesia",
   url: ".img/milesia-web.png",
   tagline: "Queen of Chaos",
   blurb: "",
 };
 
-cardCategories["eirashaol"] = {
+cardDetails["eirashaol"] = {
+  type: "parent",
+
+  category: "eirashaol-category",
   cardname: "Eira Shaol",
   url: ".img/eirashaol-web.png",
   tagline: "Queen of Rebma",
   blurb: "",
 };
-cardCategories["unknown"] = {
+cardDetails["unknown"] = {
+  type: "parent",
+
+  category: "unknown-category",
   cardname: "unknown",
   url: ".img/unknown.png",
   tagline: "Illegitimate",
   blurb: "Oberon's blyblows.",
 };
-cardCategories["lasaircaite"] = {
+cardDetails["lasaircaite"] = {
+  type: "parent",
+
+  category: "lasaircaite-category",
   cardname: "The Laisir Caite",
   url: ".img/serpentback.png",
   tagline: "The Lasair Caite",
   blurb: "People of Chaos",
 };
 
-const cardDetails = {};
 cardDetails["ophelia"] = {
+  type: "child",
   cardname: "Ophelia",
   url: "./img/ophelia.png",
-  angle: -9,
+  parent: "milesia-category",
   tagline: "Inferno",
   blurb: "",
   description: "<h2>Inferno</h2>",
@@ -60,16 +80,18 @@ cardDetails["ophelia"] = {
 
 cardDetails["bevin"] = {
   cardname: "Bevin",
+  type: "child",
   url: "./img/bevin.png",
-  angle: 6,
+  parent: "milesia-category",
   tagline: "Slayer of Monsters",
   blurb: "",
   description: "<h2>Slayer of Monsters</h2>",
 };
 cardDetails["suillir"] = {
   cardname: "Suil Lir",
+  type: "child",
   url: "./img/suillir.png",
-  angle: 3,
+  parent: "fina-category",
   tagline: "Master of Hounds",
   blurb: "",
   description: "<h2>Master of Hounds</h2>",
@@ -77,8 +99,9 @@ cardDetails["suillir"] = {
 
 cardDetails["diolinda"] = {
   cardname: "Diolinda",
+  type: "child",
   url: "./img/diolinda.png",
-  angle: -1,
+  parent: "eirashaol-category",
   tagline: "Confidante",
   blurb:
     "<p>Diolinda is many things.  Youngest child of Oberon and Eira Shaol. Patron of the arts.  Shadow Earth enthusiast.  Oberon's favorite.  She is, indeed many things, but you can rest assured that none of those things is 'spy'.",
@@ -86,8 +109,9 @@ cardDetails["diolinda"] = {
 
 cardDetails["ava"] = {
   cardname: "Ava",
+  type: "child",
   url: "./img/Ava.png",
-  angle: 6,
+  parent: "unknown-category",
   tagline: "Pirate Queen",
   blurb:
     "<p>Oberon's semi-acknowldged bastard and general black ops agent.  She has been given rulership of Minos, and most opposition to that has been quashed fairly violently.  Her relationship with her father is rocky, and her siblings are still largely an unknown.  Oberon has stated that the stars say her life will not be one of peace, and to help bear that, he has allowed her to continue to bear the pattern blade <a href='https://prosperity-amber.slab.com/posts/pattern-blades-vumfpia0'>Rekamuk</a>.",
@@ -95,8 +119,9 @@ cardDetails["ava"] = {
 
 cardDetails["borel"] = {
   cardname: "Borel",
+  type: "child",
   url: "./img/borel-web.png",
-  angle: 6,
+  parent: "milesia-category",
   tagline: "Master of the Abyss",
   blurb:
     "<p>Third child and first son of Oberon and Ophelia, Borel spends most of his time in what he describes as 'his studies' but which others refer to as 'plugging holes in the universe'.  He is one of the more sorcerously knowledgeable members of the family, but consistently defers to his hister on such matters. </a>.",
@@ -104,11 +129,12 @@ cardDetails["borel"] = {
 
 cardDetails["ciara"] = {
   cardname: "Ciara",
+  type: "child",
   url: "./img/ciara-web.png",
-  angle: 6,
+  parent: "milesia-category",
   tagline: "Burned Sorceress",
   blurb:
     "<p>Second daughter of Oberon and Ophelia, she has been Dworkin's student and currently serves as Amber's sorcerer in residence. She was married, for a time, to a lord of the Lasir Caite, and her children have gotten old enough to start taking an interest in Amber.</a>.</p>",
 };
 
-export { cardDetails, cardCategories };
+export { cardDetails };
